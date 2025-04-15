c52eecc5d3143bd90055b34a5fba1ad03e99b8d0
vitorgil8 <ve3792682@gmail.com>
2025-04-15 15:15:15 -0300

    inicio do header #2

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,40 @@
 </script>
 
 <template>
-  
+<main>
+  <header>
+    <div>
+      <p>IFbooks</p>
+      <p class="apreco">Apreço a leitura</p>
+    </div>
+    <div>
+      
+    </div>
+    <nav>
+      <div>
+        <ul>
+          <li>
+            <a href="#">Termos</a>
+          </li>
+          <li>
+            <a href="#">Devoluções</a>
+          </li>
+          <li>
+            <a href="#">Envio</a>
+          </li>
+          <li>
+            <a href="#">Equipe</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <section class="inico">
+    <h1>
+      
+    </h1>
+  </section>
+</main>  
 </template>
 
 <style scoped>
