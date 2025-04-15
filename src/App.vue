a11cfcaf7dce79c3da648c94f31a522b52731a56
vitorgil8 <ve3792682@gmail.com>
2025-04-15 16:57:40 -0300

    #continuação do projeto  #3

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <script setup>
-
+src="https://kit.fontawesome.com/14e2fd4a9f.js" crossorigin="anonymous"
 </script>
 
 <template>
 <main>
   <header>
-    <div>
-      <p>IFbooks</p>
-      <p class="apreco">Apreço a leitura</p>
-    </div>
-    <div>
-      
-    </div>
-    <nav>
+    <div class="topo">
       <div>
+        <p>IFbooks</p>
+        <p class="apreco">Apreço a leitura</p>
+      </div>
+    <div class="itens">
         <ul>
           <li>
             <a href="#">Termos</a>
@@ -28,15 +25,37 @@
             <a href="#">Equipe</a>
           </li>
         </ul>
-      </div>
-    </nav>
+    </div>
+    </div>
   </header>
   <section class="inico">
-    <h1>
-      
-    </h1>
+
+  <div class="pagina1">
+        <div class="mudar">
+          <p>Autor de Abril</p>
+          <h1>Eric-Emanuel Schmitt </h1>
+          <p>Eric-Emmanuel Schmitt has been awarded more than 20 <br> literary prizes and distinctions, and in 2001 he received the <br> title of Chevalier des Arts et des Lettres. His books have been <br> translated into over 40 languages.</p>
+          <button>Acessar página do livro</button>
+        </div>  
+        <div>
+          <img src="/public/imagens/book.png" alt="book.png" style="display: block; margin: ;">
+        </div>
+  </div> 
   </section>
-</main>  
+  <section class="mais">
+  <ul>
+      <li>
+        Frete grátis para SC
+      </li>
+      <li>
+        Livros recomendados
+      </li>
+      <li>
+        Mais vendidos
+      </li>
+  </ul>
+  </section>
+</main> 
 </template>
 
 <style scoped>
